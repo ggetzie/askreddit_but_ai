b3452e70a266e2d93ec2b6a704b48fb3dc09e52d
Gabe Getzie <ggetzie@gmail.com>
2020-07-23 19:48:57 +0800

    titles downloaded, generated

--- get_titles.py
+++ get_titles.py
@@ -1,19 +1,39 @@
 import os
-
 import requests
-import requests.auth
+import time
 
-client_id = os.environ["REDDIT_CLIENT_ID"]
-client_secret = os.environ["REDDIT_CLIENT_SECRET"]
-REDIRECT_URL = "http://localhost:65010/reddit_callback"
+headers = {"User-agent": "AskReddit GPT Bot"}
+params = {"t": "all"}
 
-def get_token(code):
-    client_auth = requests.auth.HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET)
-    post_data = {"grant_type": "authorization_code",
-                 "code": code,
-         "redirect_uri": REDIRECT_URI}
-    response = requests.post("https://ssl.reddit.com/api/v1/access_token",
-                             auth=client_auth,
-         data=post_data)
-    token_json = response.json()
-    return token_json["access_token"]
+
+if __name__ == "__main__":
+     titles = set()
+     pcount = 0
+     last_total = 0
+     times = ("hour", "day", "week", "month", "year", "all")
+     sorts = ("top", "hot", "new", "rising", "controversial")
+     for sort_ in sorts:
+          url = f"https://www.reddit.com/r/AskReddit/{sort_}/.json"
+          for time_ in times:
+               params["t"] = time_
+               if "count" in params: 
+                    del params["count"]
+                    pcount = 0
+               for i in range(30):
+                    if len(titles) > 10000: break
+                    r = requests.get(url, headers=headers, params=params)
+                    print(f"Getting {r.url}")
+                    if not r.status_code == 200:
+                         print(f"Error: {r.text}")
+                         break
+                    d = r.json()
+                    for item in d["data"]["children"]:
+                         titles.add(item["data"]["title"])
+                    params["after"] = d["data"]["after"]
+                    pcount += len(d["data"]["children"])
+                    params["count"] = pcount
+                    with open("titles.txt", "w") as outfile:
+                         outfile.write("\n".join(titles))
+                    print(f"{len(titles)} titles collected so far")
+                    last_total = len(titles)
+                    time.sleep(2)
