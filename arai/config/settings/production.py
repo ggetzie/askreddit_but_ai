1ad935e607b3f002cf211228fe7f15a699875094
gabe <ggetzie@gmail.com>
2020-08-08 16:56:39 +0800

    csrf cookie domain

--- arai/config/settings/production.py
+++ arai/config/settings/production.py
@@ -25,6 +25,9 @@ SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)
 SESSION_COOKIE_SECURE = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-secure
 CSRF_COOKIE_SECURE = True
+
+CSRF_TRUSTED_ORIGINS = ["d19met9wl0b83b.cloudfront.net", ".askredditbut.ai"]
+
 # https://docs.djangoproject.com/en/dev/topics/security/#ssl-https
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-seconds
 # TODO: set this to 60 seconds first and then to 518400 once you prove the former works
