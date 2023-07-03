0d1db0f4bad1043e08117744911730d7a97a36e6
Gabriel Getzie <ggetzie@hku.hk>
2023-07-03 14:48:00 +0800

    change timezone to UTC

--- arai/config/settings/base.py
+++ arai/config/settings/base.py
@@ -19,7 +19,7 @@ DEBUG = env.bool("DJANGO_DEBUG", False)
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # though not all of them may be available with every OS.
 # In Windows, this must be set to your system time zone.
-TIME_ZONE = "America/New_York"
+TIME_ZONE = "UTC"
 # https://docs.djangoproject.com/en/dev/ref/settings/#language-code
 LANGUAGE_CODE = "en-us"
 # https://docs.djangoproject.com/en/dev/ref/settings/#site-id
